a0fa89d1b379aa3fe87fb9b8800bf97444651cb7
jinseung0327 <127307160+jinseung0327@users.noreply.github.com>
2023-11-10 22:50:21 +0900

    Update subnet.tf

--- subnet.tf
+++ subnet.tf
@@ -12,7 +12,7 @@ resource "aws_subnet" "wsi-subnet-public-a" {
 resource "aws_subnet" "wsi-subnet-public-b" {
   vpc_id = aws_vpc.wsi-vpc.id
   cidr_block = "10.0.1.0/24"
-  availability_zone = "ap-northeast-2a"
+  availability_zone = "ap-northeast-2b"
   map_public_ip_on_launch = true
 
   tags = {
@@ -34,7 +34,7 @@ resource "aws_subnet" "wsi-subnet-private-a" {
 resource "aws_subnet" "wsi-subnet-private-b" {
   vpc_id = aws_vpc.wsi-vpc.id
   cidr_block = "10.0.3.0/24"
-  availability_zone = "ap-northeast-2a"
+  availability_zone = "ap-northeast-2b"
   map_public_ip_on_launch = true
 
   tags = {
