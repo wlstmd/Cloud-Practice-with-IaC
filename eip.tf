0ae7a921030e18e72c377a5cbb06e001eadf11d6
jinseung0327 <127307160+jinseung0327@users.noreply.github.com>
2023-11-11 02:10:34 +0900

    Update eip.tf

--- eip.tf
+++ eip.tf
@@ -1,13 +1,13 @@
 resource "aws_eip" "wsi-nat-eip" {
   domain = "vpc"
   tags = {
-    Name = "wsi-nat-eip"
+    Name = "-"
   }
 }
 
 resource "aws_eip" "wsi-nat-eip2" {
   domain = "vpc"
   tags = {
-    Name = "wsi-nat-eip2"
+    Name = "-"
   }
 }
